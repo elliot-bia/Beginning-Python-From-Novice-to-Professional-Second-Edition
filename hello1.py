e1a5cbe2240bda9cb88e4f7bc0bd64ebe8a29fcd
elliot-bia <elliot.bia.8989@outlook.com>
2019-03-09 13:54:40 +0800

    第二次尝试提交代码

--- hello1.py
+++ hello1.py
@@ -1,2 +1,15 @@
-name=input("What is your name?")
-print ("hello,"+name+'!')
+#!/usr/bin/env python
+# coding:utf-8
+# 51memo.1.py
+# A memo demo
+# author:Elliot
+
+__author__ = 'Elliot'
+
+import datetime  # 系统优先，其次第三方库，最后自定义
+                 # 注释后面要有1个空格，注释前2个空格
+                 # 函数命名一般用下划线
+                 # 类命名首字母大写
+
+# TODO：todo是为了继续完成某个代码
+desc = 
